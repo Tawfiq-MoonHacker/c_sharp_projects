1dfc1a98b510ae46fe2395cf406eebbad6d0ba83
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2022-03-12 13:35:41 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string c = "you";
+            Console.WriteLine(c.Length);
+            string s = c.Substring(0, 2);
+            Console.WriteLine(s);
+            string c1 = "you";
+            Console.WriteLine(string.Concat(c1, s));
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = "hello there";
+            string [] p = s.Split(' ');
+            int charcount = 0;
+            Console.WriteLine("CHAR count: " + s.Length);
+            Console.WriteLine("Counting words" + (p.Length-1));
+
+
+            Console.Read();
+
+        }
+    }
+}
