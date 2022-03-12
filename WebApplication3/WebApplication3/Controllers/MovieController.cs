d65435707285b8fc7f1b9d4f78b4fdfe7940dd10
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2022-03-12 14:10:32 +0300

    Add files via upload

---
+++ WebApplication3/WebApplication3/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models.Movie;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult GetMovies()
+        {
+            Movie obj = new Movie();
+
+            List<Movie> movies = obj.GetAllMovies();
+            var vm = new MovieViewModel();
+            vm.MoviesDetails = new List<MovieDetailsViewModel>();
+
+            foreach(Movie movie in movies)
+            {
+                var movievm = new MovieDetailsViewModel();
+                movievm.Name = movie.Name;
+                movievm.Rating = movie.Rating;
+                movievm.Genera = movie.Genera;
+                movievm.Id = movie.Id;
+                vm.MoviesDetails.Add(movievm);
+
+            }
+
+            return View("AllMovies",vm);
+
+        }
+    }
+}
