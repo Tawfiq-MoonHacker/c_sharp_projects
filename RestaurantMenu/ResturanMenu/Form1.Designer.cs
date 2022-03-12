3b17b4d3c5e24080cbbd127dbcf48b51163b188d
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2022-03-12 13:46:59 +0300

    Add files via upload

---
+++ RestaurantMenu/ResturanMenu/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿using System;
+
+namespace ResturanMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hamburger",
+            "Crispy Fried Chicken",
+            "Pizza",
+            "Steak",
+            "Chocolate Chip Cookie",
+            "Taco"});
+            this.comboBox1.Location = new System.Drawing.Point(243, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Food";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ingredients";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tax 16%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Total";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "None",
+            "25%",
+            "50%",
+            "75%",
+            "100%"});
+            this.comboBox2.Location = new System.Drawing.Point(243, 64);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(93, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.Text = "Coupon";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(141, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "$";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(141, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "$";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(141, 287);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "$";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 182);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(396, 39);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(459, 325);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(22, 331);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Calories";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(103, 330);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 376);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Restaurant Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ RestaurantMenu/ResturanMenu/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.Build.Framework.XamlTypes;
+using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
+using System.Windows.Forms.DataVisualization.Charting;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+
+namespace ResturanMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double price;
+        public double tax = 0.16;
+        public double coupon = 1.0;
+        public int calories;
+        public void ChangeLabels()
+        {
+            label3.Text = "" + price;
+            label4.Text = "" + price * tax;
+            label5.Text = "" + price * (1 + tax) * coupon;
+            label11.Text = "" + calories;
+        }
+        public void ClearChart()
+        {
+            chart1.Titles.Clear();
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem == "Hamburger")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.Hamburger;
+                price = 4.5;
+                calories = 295;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("Hamburger Nutrition (100 Grams)");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Potasium", "0.226");
+                chart1.Series["Series1"].Points.AddXY("Sodium", "0.414");
+                chart1.Series["Series1"].Points.AddXY("Cholestrol", "0.047");
+                chart1.Series["Series1"].Points.AddXY("Fat", "14");
+                chart1.Series["Series1"].Points.AddXY("Carbohydrate", "24");
+                chart1.Series["Series1"].Points.AddXY("Protein", "17");
+
+            }
+            if(comboBox1.SelectedItem == "Crispy Fried Chicken")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.crispyfriedchicken;
+                price = 3.5;
+                calories = 300;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("Crispy Fried Chicken Nutrition per 100 Grams");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Sodium", "4.5");
+                chart1.Series["Series1"].Points.AddXY("Iron", "0.0036");
+                chart1.Series["Series1"].Points.AddXY("Fat", "14");
+                chart1.Series["Series1"].Points.AddXY("Carbohydrate", "70");
+                chart1.Series["Series1"].Points.AddXY("Protein", "10");
+
+            }
+            if(comboBox1.SelectedItem == "Pizza")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.Pizaa;
+                price = 7.99;
+                calories = 460;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("One large slice Nutrition (167 grams)");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Sugar", "1");
+                chart1.Series["Series1"].Points.AddXY("Sodium", "0.9");
+                chart1.Series["Series1"].Points.AddXY("Fat", "26");
+                chart1.Series["Series1"].Points.AddXY("Carbs", "37");
+                
+
+            }
+            if(comboBox1.SelectedItem == "Steak")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.Steak;
+                price = 8.00;
+                calories = 179;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("Steak Nutrition (85 Grams)");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Sodium", "0.049");
+                chart1.Series["Series1"].Points.AddXY("Fat", "7.6");
+                chart1.Series["Series1"].Points.AddXY("Protein", "26");
+
+            }
+            if(comboBox1.SelectedItem == "Chocolate Chip Cookie")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.ChocolateChip;
+                price = 1.99;
+                calories = 148;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("Cookies Nutrition (30 Grams)");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Potasium", "0.051");
+                chart1.Series["Series1"].Points.AddXY("Sodium", "0.093");;
+                chart1.Series["Series1"].Points.AddXY("Fat", "7.4");
+                chart1.Series["Series1"].Points.AddXY("Carbohydrate", "20");
+                chart1.Series["Series1"].Points.AddXY("Protein", "1.5");
+                chart1.Series["Series1"].Points.AddXY("Caffeine", "0.0033");
+
+
+            }
+            if(comboBox1.SelectedItem == "Taco")
+            {
+                pictureBox1.Image = ResturanMenu.Properties.Resources.Taco;
+                price = 3.99;
+                calories = 210;
+                ChangeLabels();
+
+                ClearChart();
+
+                chart1.Titles.Add("Taco Nutrition (102 Grams)");
+                chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+                chart1.Series["Series1"].Points.AddXY("Potasium", "0.164");
+                chart1.Series["Series1"].Points.AddXY("Sodium", "0.571");
+                chart1.Series["Series1"].Points.AddXY("Cholestrol", "0.026");
+                chart1.Series["Series1"].Points.AddXY("Fat", "9.9");
+                chart1.Series["Series1"].Points.AddXY("Carbohydrate", "21");
+                chart1.Series["Series1"].Points.AddXY("Protein", "9.4");
+
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Taco")
+            {
+                MessageBox.Show("A taco is a traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling. The tortilla is then folded around the filling and eaten by hand.", "Taco Ingredients", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            if (comboBox1.SelectedItem == "Chocolate Chip Cookie")
+            {
+                MessageBox.Show("A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips or chocolate morsels as its distinguishing ingredient. Circa 1938, Ruth Graves Wakefield added chopped up bits from a Nestlé semi-sweet chocolate bar into a cookie.", "Cookie Ingredients", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            if (comboBox1.SelectedItem == "Pizza")
+            {
+                MessageBox.Show("Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.", "Pizza Ingredients", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            if (comboBox1.SelectedItem == "Hamburger")
+            {
+                MessageBox.Show("A hamburger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, grilled, smoked or flame broiled.", "Hamburger Ingredients", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            if (comboBox1.SelectedItem == "Crispy Fried Chicken")
+            {
+                MessageBox.Show("Crispy fried chicken is a standard dish in the Cantonese cuisine of southern China and Hong Kong. The chicken is fried in such a way that the skin is extremely crunchy, but the white meat is relatively soft.", "Crispy Fried Chicken Ingredients", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == "25%")
+            {
+                coupon = 0.75;
+                ChangeLabels();
+            }
+            if (comboBox2.SelectedItem == "50%")
+            {
+                coupon = 0.50;
+                ChangeLabels();
+            }
+            if (comboBox2.SelectedItem == "75%")
+            {
+                coupon = 0.25;
+                ChangeLabels();
+            }
+            if (comboBox2.SelectedItem == "100%")
+            {
+                coupon = 0.0;
+                ChangeLabels();
+            }
+            if(comboBox2.SelectedItem == "None")
+            {
+                coupon = 1.0;
+                ChangeLabels();
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
